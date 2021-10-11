afa63cfe33a2e89126cbefcee08885faac0d2913
NomedXD <coovshow1@gmail.com>
2021-10-11 20:48:58 +0300

    Изменил название на более преемлимое

--- src/main/java/hc/MainMenu.java
+++ src/main/java/hc/MainMenu.java
@@ -9,7 +9,7 @@ import java.awt.event.ActionListener;
 public class MainMenu extends JFrame {
 
     public MainMenu() {
-        this.setTitle("Health calculator 1.0");
+        this.setTitle("Health calculator MegaBetaTest777 0.000001");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(800, 600);
         this.setLocation(500, 200);
