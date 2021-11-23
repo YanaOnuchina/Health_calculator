25f53c9568d0922bf885df410012cdf77a55f0b8
NomedXD <coovshow1@gmail.com>
2021-11-23 20:03:51 +0300

    Главное окно

--- src/main/java/hc/newday.java
+++ src/main/java/hc/newday.java
@@ -6,11 +6,11 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class newday extends JDialog{
-    ArrayList<String> productName;
-    ArrayList<Double> calories;
+
     int i;
     double calculate =0;
-    int length;
+    double val;
+
 
     public newday(ArrayList<String> productName,ArrayList<Double> calories,float norma) {
         this.setSize(800, 600);
@@ -22,6 +22,25 @@ public class newday extends JDialog{
         panel.setLayout(new GridBagLayout());
         getContentPane().add(panel);
 
+
+        JTextField proverka = new JTextField();
+        proverka.setBounds(480,300,120,40);
+        proverka.setEditable(true);
+        this.add(proverka);
+
+
+        JLabel sravn = new JLabel();
+        this.add(sravn);
+        sravn.setBounds(145,30,250,80);
+
+        JLabel sravn1 = new JLabel();
+        this.add(sravn1);
+        sravn1.setBounds(145,20,250,80);
+
+        JLabel text = new JLabel();
+        this.add(text);
+        text.setBounds(250,70,250,80);
+
         JButton cancel = new JButton("<html><h2><font color=\"black\">Назад");
         cancel.setBounds(20, 20, 100, 40);
         this.add(cancel);
@@ -32,15 +51,13 @@ public class newday extends JDialog{
         this.add(Calculate);
         Calculate.addActionListener(e -> {
             if (calculate <= norma) {
-                JLabel sravn = new JLabel("<html><h2><font color=\"black\">молодец");
-                this.add(sravn);
-                sravn.setBounds(145,40,60,80);
-            }else {JLabel sravn = new JLabel("<html><h2><font color=\"black\">ты лох");
-                this.add(sravn);
-                sravn.setBounds(145,40,60,80);}
-
+                text.setText("<html><h2><font color=\"black\">молодец");
+                proverka.setText(Double.toString(norma-calculate));
+            }else {
+                text.setText("<html><h2><font color=\"black\">худей");
+                proverka.setText(Double.toString(calculate-norma));
+            }
         });
-
         DefaultListModel<String> model = new DefaultListModel<>();
         for(int i = 0; i < productName.size(); i++){
             String string = String.format(productName.get(i));
@@ -56,20 +73,22 @@ public class newday extends JDialog{
         ncalories.setEditable(true);
         this.add(ncalories);
 
+
+
         JButton saveb = new JButton("<html><h2><font color=\"black\">Добавить");
-        saveb.setBounds(400, 125, 100, 40);
+        saveb.setBounds(400, 125, 120, 40);
         this.add(saveb);
-        /*if (ncalories.getText().equals("")){
-            saveb.setEnabled(false);}else{
-            saveb.setEnabled(true);
-        }*/
         saveb.addActionListener(e -> {
-            i = list1.getSelectedIndex();
-            calculate =calculate+ calories.get(i)*Integer.parseInt(ncalories.getText());
+           try{ i = list1.getSelectedIndex();}catch(Exception m){}
+            if (i == -1)  {
+                sravn.setText("Вы не выбрали ни одного продукта");
+
+            }
+            try{val = Double.parseDouble(ncalories.getText());} catch (Exception k){sravn1.setText("Вы ввели неверное значение в поле");}
+            calculate =calculate+ (calories.get(i)/100)*val;
+            sravn1.setText("");
+            sravn.setText("");
         });
-
-
-
 
         this.setLayout(null);
     }
